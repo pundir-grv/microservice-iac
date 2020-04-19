eb9515b2aec3f8eec02df54e84c4b8d77ba4d5cd
Gaurav Pundir <pundirgrv1993@gmail.com>
2020-04-19 21:15:54 +0530

    changing psycopg2 pkg

--- lambda_scripts/kong_setup.py
+++ lambda_scripts/kong_setup.py
@@ -1,10 +1,22 @@
+import sys
+sys.path.insert(0,"./psycopg2")
 import psycopg2
 from get_rds_creds import get_secret, get_dbDetails
 
-def main():
+def main(event, context):
     dbEndpoint, dbName = get_dbDetails()
     dbCreds = get_secret()
-    print(dbEndpoint, dbName, dbCreds)
-
-if __name__ == "main":
-    main()
+    conn = psycopg2.connect(
+        host=dbEndpoint.split(":")[0],
+        port=dbEndpoint.split(":")[1],
+        database=dbName, 
+        user=dbCreds["username"], 
+        password=dbCreds["password"]
+    )
+    cur = conn.cursor()
+    print('PostgreSQL database version:')
+    cur.execute('SELECT version()')
+    db_version = cur.fetchone()
+    print(db_version)
+    cur.close()
+    
